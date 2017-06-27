d89160d1f2cf6bfda3333db4aabd92af9a84fa59
Sarah Dueweke <sdueweke@gmail.com>
2017-06-28 11:41:05 +1200

    three passing tests

--- tests/client/actions.test.js
+++ tests/client/actions.test.js
@@ -1,17 +1,27 @@
 import test from 'ava'
 import nock from 'nock'
 
-import * as action from '../../client/actions'
-
+import * as catActions from '../../client/actions/categories'
+import * as recActions from '../../client/actions/recipes'
 
 test.cb('getCategories', t => {
   const scope = nock('http://localhost:80')
     .get('/api/categories')
     .reply(200)
 
-    action.getCategories()((actual) => {
-      console.log(actual);
+    catActions.getCategories()((actual) => {
       t.is(actual.type, 'RECEIVE_CATEGORIES')
+      t.end()
+    })
+})
+
+test.cb('getRecipes', t => {
+  const scope = nock('http://localhost:80')
+    .get('/api/recipes')
+    .reply(200)
+
+    recActions.getRecipes()((actual) => {
+      t.is(actual.type, 'RECEIVE_RECIPES')
       t.end()
     })
 })
